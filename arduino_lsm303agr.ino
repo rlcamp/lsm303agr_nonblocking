5d12fba8d82db6c737f55d351c850cf694fe3259
Richard Campbell <rlcamp@users.noreply.github.com>
2023-02-22 15:09:57 -0800

    Example of using accelerometer for pitch/roll compensation of heading estimate

--- arduino_lsm303agr.ino
+++ arduino_lsm303agr.ino
@@ -16,6 +16,12 @@ void setup() {
     prev = millis();
 }
 
+static void int_cross_product(int32_t c[3], const int16_t a[3], const int16_t b[3]) {
+    c[0] = a[1] * b[2] - a[2] * b[1];
+    c[1] = a[2] * b[0] - a[0] * b[2];
+    c[2] = a[0] * b[1] - a[1] * b[0];
+}
+
 void loop() {
     __WFI();
     const unsigned long now = millis();
@@ -37,16 +43,40 @@ void loop() {
             if (result.mag[2] < z_min) z_min = result.mag[2];
             if (result.mag[2] > z_max) z_max = result.mag[2];
 
-            const int x_calibrated = result.mag[0] - (x_min + x_max + 1) / 2;
-            const int y_calibrated = result.mag[1] - (y_min + y_max + 1) / 2;
+            /* TODO: validate against integer overflow */
+            const int16_t x_offset = (x_min + x_max + 1) / 2;
+            const int16_t y_offset = (y_min + y_max + 1) / 2;
+            const int16_t z_offset = (z_min + z_max + 1) / 2;
+
+            /* we should have a guarantee these don't overflow */
+            const int16_t mag_with_offset[3] = {
+              (int16_t)(result.mag[0] - x_offset),
+              (int16_t)(result.mag[1] - y_offset),
+              (int16_t)(result.mag[2] - z_offset)
+            };
+
+            /* left handed cross product */
+            int32_t east[3];
+            int_cross_product(east, result.accel, mag_with_offset);
 
             /* this value will be bs until the sensor has been turned in a circle */
-            const float heading = atan2f(y_calibrated, x_calibrated) * 180.0 / M_PI;
+            /* TODO: validate both of these away from magnets */
+            const float heading0 = remainderf(atan2f(-mag_with_offset[1], mag_with_offset[0]) * 180.0f / (float)M_PI - 180.0f, 360.0f) + 180.0f;
+            const float heading1 = remainderf(atan2f(east[0], east[1]) * 180.0f / (float)M_PI - 180.0f, 360.0f) + 180.0f;
 
-            Serial.printf("accel: %d %d %d\n", result.accel[0], result.accel[1], result.accel[2]);
-            Serial.printf("mag: %d %d %d: heading %dd, strength %d\n",
-                          result.mag[0], result.mag[1], result.mag[2], (int)(heading),
-                          (int)sqrtf(result.mag[0] * result.mag[0] + result.mag[1] * result.mag[1] + result.mag[2] * result.mag[2]));
+            /* assuming +x is "forward", this gives positive angles when pitched up */
+            const float pitch = atan2f(result.accel[0], result.accel[2]) * 180.0 / M_PI;
+
+            /* assuming +x is "forward", this gives positive angles when rolled right */
+            const float roll = atan2f(-result.accel[1], result.accel[2]) * 180.0 / M_PI;
+
+            Serial.printf("pitch %dd, roll %dd\n", (int)lrint(pitch), (int)lrint(roll));
+
+            Serial.printf("accel: % +5d % +5d % +5d\n", result.accel[0], result.accel[1], result.accel[2]);
+            Serial.printf("mag raw: % +5d % +5d % +5d\n", result.mag[0], result.mag[1], result.mag[2]);
+            Serial.printf("mag offsets % +5d % +5d % +5d, calibrated: % +5d % +5d % +5d\n",
+                          x_offset, y_offset, z_offset, mag_with_offset[0], mag_with_offset[1], mag_with_offset[2]);
+            Serial.printf("heading: magnetic only %dd, pitch/roll compensated %dd\n", (int)lrint(heading0), (int)lrint(heading1));
         }
         else Serial.printf("no values yet\n");
 
